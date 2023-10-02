73870cd020cc70c7920b1088917eb2c746077b3f
Vaishnavi Bharti <vaishuridhu@gmail.com>
2023-10-02 16:03:29 +0530

    datatypes variable

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert (3+3) we are using node js not browser 
+console.log(3+3);
+
+console.log("vaishu");  // code readability should be high 
+
+let name = "vaishu";
+let age = "22" ;
+let isLoggedIn = false;
+let state;
+let country = null ;
+
+//number =>2 to power 53
+//binint 
+//strinh =>""
+
+//boolean => true/false
+//null=> standalone value
+// undefined=> value is not assigned
+// symbol => unique
+
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ JavaScript/variable.js
@@ -0,0 +1,14 @@
+const accountId = 142;
+let accountEmail = "vaishurodhu@gmail.com";
+var accountPassword = "12345";
+
+acccountCity = "Noida";
+let accountState;
+// accountId = 2
+
+// console.log(accountId);
+/*prefer  not to use var  */
+accountEmail ="hc@hc.com"
+accountPassword ="123"
+acccountCity="banglore"
+console.table([accountId,accountEmail,accountPassword,acccountCity,accountState]);
